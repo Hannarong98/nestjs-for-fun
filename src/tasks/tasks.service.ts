c8acf7afe98c9c40ae0c76c8a6bd358bd7ae8021
Hannarong Klinjan <hannarong98@hotmail.com>
2021-09-06 21:08:01 +0200

    Enforce return types

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,7 +5,7 @@ import { Task } from './task.model';
 export class TasksService {
   private tasks: Task[] = [];
 
-  public getAllTasks() {
+  public getAllTasks(): Task[] {
     return this.tasks;
   }
 }
