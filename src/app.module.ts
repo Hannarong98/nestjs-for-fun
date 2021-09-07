02b9458cfc8ecc6a4092d7545356960d8e086f24
Hannarong Klinjan <hannarong98@hotmail.com>
2021-09-07 23:49:13 +0200

    Configure typeorm to use local postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const { PG_USERNAME, PG_PASSWORD, PG_DATABASE_NAME } = process.env;
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: PG_USERNAME,
+      password: PG_PASSWORD,
+      database: PG_DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
