603732268d1ec316a03997a9c844ae24e78f4c1d
Hannarong Klinjan <hannarong98@hotmail.com>
2021-09-08 22:50:11 +0200

    Add interceptor to intercept unmarshalling of data

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { useRequestLogging } from './middlewares/logger.middlewares';
+import { TransformInterceptor } from './utils/transform.interceptor';
 async function bootstrap() {
   const { PORT } = process.env;
 
   const app: INestApplication = await NestFactory.create(AppModule);
   useRequestLogging(app);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(PORT);
 }
 bootstrap();
